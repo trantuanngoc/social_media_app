4e0de1b568397607721d69a0aae662214fa4c332
fancol <ngocthoson1@gmail.com>
2020-05-28 02:42:41 -0700

    Fixed heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get '/home',to:'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get '/about',to:'static_pages#about'
   get '/contact',to:'static_pages#contact'
